7d3bba939b544e97eb58e19a9f6066eeeb9c75d1
Sofia <itssonyamisyura@gmail.com>
2026-03-10 14:49:26 +0100

    name and email done with useFormik

--- src/Form.js
+++ src/Form.js
@@ -1,46 +1,103 @@
+import { useFormik } from "formik";
 
+const validate = values => {
+    const errors = {};
+
+    if (!values.name) {
+        errors.name = 'Required field';
+    } else if (values.name.length < 2) {
+        errors.name = 'At least 2 characters'
+    }
+
+    if (!values.email) {
+        errors.email = 'Required field';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+        errors.email =  'Invalid email address';
+    }
+
+    return errors; // returns obj with errors(or an empty obj)
+}
+
+// handleChange is called each time we change our input
+// then validate() and inside this function => returns obj with errors(or an empty obj) --> using handleBlur(formik.touched)
 
 const Form = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            amount: 0,
+            currency: '',
+            text: '',
+            terms: false
+        },
+        validate,
+        onSubmit: values => console.log(JSON.stringify(values, null, 2))
+    });
+
     return (
-        <form className="form">
-            <h2>Отправить пожертвование</h2>
-            <label htmlFor="name">Ваше имя</label>
+        <form className="form" onSubmit={formik.handleSubmit}>
+            <h2>Send a donation</h2>
+            <label htmlFor="name">Your name</label>
             <input
                 id="name"
                 name="name"
                 type="text"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
             />
-            <label htmlFor="email">Ваша почта</label>
+            {formik.errors.name && formik.touched.name ? <div>{formik.errors.name}</div> : null}
+            <label htmlFor="email">Your email</label>
             <input
                 id="email"
                 name="email"
                 type="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
             />
-            <label htmlFor="amount">Количество</label>
+            {formik.errors.email && formik.touched.email ? <div>{formik.errors.email}</div> : null}
+            <label htmlFor="amount">Amount</label>
             <input
                 id="amount"
                 name="amount"
                 type="number"
+                value={formik.values.amount}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
             />
-            <label htmlFor="currency">Валюта</label>
+            <label htmlFor="currency">Currency</label>
             <select
                 id="currency"
-                name="currency">
-                    <option value="">Выберите валюту</option>
+                name="currency"
+                value={formik.values.currency}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}>
+                    <option value="">Select currency</option>
                     <option value="USD">USD</option>
-                    <option value="UAH">UAH</option>
-                    <option value="RUB">RUB</option>
+                    <option value="CZK">CZK</option>
+                    <option value="EUR">EUR</option>
             </select>
-            <label htmlFor="text">Ваше сообщение</label>
+            <label htmlFor="text">Your message</label>
             <textarea 
                 id="text"
                 name="text"
+                value={formik.values.text}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
             />
             <label className="checkbox">
-                <input name="terms" type="checkbox" />
-                Соглашаетесь с политикой конфиденциальности?
+                <input 
+                    name="terms" 
+                    type="checkbox"
+                    value={formik.values.terms}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur} />
+                Do you agree with the privacy policy?
             </label>
-            <button type="submit">Отправить</button>
+            <button type="submit">Submit</button>
         </form>
     )
 }
